640011ff5618c55c90c777e1e9e9ca2d977d5540
naxus28 <gabrielferraz27@gmail.com>
2017-07-01 12:00:05 -0400

    adds Machine package

---
+++ Machine/src/Toaster2Test.java
@@ -0,0 +1,15 @@
+
+public class Toaster2Test {
+
+	public static void main(String[] args) {
+		Toaster2 toaster = new Toaster2();
+		toaster.heatUp();
+		toaster.coolDown();
+		toaster.turnOn();
+		toaster.heatUp();
+		toaster.coolDown();
+		toaster.turnOff();
+
+	}
+
+}
